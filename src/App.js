e781909393e0990eb250498e345a45a481b05d64
unknown <toliguemi94@gmail.com>
2023-12-20 20:26:52 -0300

    OLA

--- src/App.js
+++ src/App.js
@@ -2,26 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
+import injectContext from "./store/context.js";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Welcome from "./views/Welcome";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route render={() => <h1>Not found!</h1>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
 
-export default App;
+export default injectContext(App);
---
+++ src/views/Welcome.js
@@ -0,0 +1,5 @@
+function Welcome(){
+    return <h1>HOLAAAA</h1>
+}
+
+export default Welcome
