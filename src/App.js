594f5508dcaa990048459c358135c87e5d8b2155
Fourt26 <thomyfourt@gmail.com>
2023-12-22 12:10:30 -0300

    Navbar

--- src/App.js
+++ src/App.js
@@ -3,19 +3,20 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import injectContext from "./store/context.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Welcome from "./views/Welcome";
-import Footer from "./components/Footer.jsx"
-import './custom.css';
-
+import Footer from "./components/Footer.jsx";
+import "./custom.css";
+import Navbar from "./components/Navbar.jsx";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Welcome />} />
           <Route render={() => <h1>Not found!</h1>} />
         </Routes>
-        <Footer/>
+        <Footer />
       </BrowserRouter>
     </div>
   );
