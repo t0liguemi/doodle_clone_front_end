a53764e731327eb6d89091c9af7f99b440cf7aa4
unknown <toliguemi94@gmail.com>
2023-12-26 21:39:04 -0300

    hola

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import injectContext from "./store/context.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Welcome from "./views/Welcome";
 import Footer from "./components/Footer.jsx"
 import './custom.css';
 
@@ -12,7 +11,6 @@ function App() {
     <div className="App d-flex flex-column min-vh-100">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Welcome />} />
           <Route render={() => <h1>Not found!</h1>} />
         </Routes>
         <Footer/>
