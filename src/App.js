9c6cec2fd4ad0539b25e869f9bbbb469bad93778
Luis <luisalfonsocc@gmail.com>
2023-12-27 20:20:50 -0400

    Event List Listo

--- src/App.js
+++ src/App.js
@@ -2,20 +2,13 @@ import "bootstrap/dist/js/bootstrap.min.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import injectContext from "./store/context.js";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-<<<<<<< HEAD
-import Welcome  from "./views/WelcomeSplash.js";
-import "./custom.css";
-import Footer from "./components/Footer.jsx"
-import CreateEvent from "./views/CreateEvent.js";
-import EventList from "./views/EventList.js";
-=======
 import Footer from "./components/Footer.jsx"
 import CreateEvent from "./views/CreateEvent.js";
 import Welcome  from "./views/WelcomeSplash.js";
 import "./custom.css";
+import EventList from "./views/EventList.js"; 
 
 
->>>>>>> a40d72bd749c0a72cb77e0d4d6bf6de9ff3a5c74
 
 
 function App() {
--- src/views/EventList.js
+++ src/views/EventList.js
@@ -1,12 +1,12 @@
-// EventList.js
+
 import React from 'react';
-import './EventList.css'; // Asegúrate de ajustar la ruta según la ubicación real de tu archivo de estilos
+import './EventList.css'; 
 
 const eventos = [
   {
-    nombre: 'Evento 1',
+    nombre: 'Cumple Fer',
     periodo: '01/01/2023 - 02/01/2023',
-    lugar: 'Lugar 1',
+    lugar: 'Irarrazaval 4500, Ñuñoa',
     estado: 'Organizado por mí',
     respuestas: {
       aceptadas: 5,
@@ -14,9 +14,9 @@ const eventos = [
     },
   },
   {
-    nombre: 'Evento 2',
+    nombre: 'Reunion Planificacion 2024',
     periodo: '03/01/2023 - 04/01/2023',
-    lugar: 'Lugar 2',
+    lugar: 'Aponquindo 3820, Las Condes',
     estado: 'Respondido',
     respuestas: {
       aceptadas: 8,
@@ -24,45 +24,45 @@ const eventos = [
     },
   },
   {
-    nombre: 'Evento 3',
+    nombre: 'Ensayo y Copete',
     periodo: '05/01/2023 - 06/01/2023',
-    lugar: 'Lugar 3',
-    estado: 'Pendiente',
+    lugar: 'Vicuña Mackena 1500',
+    estado: 'Pendientes',
     respuestas: {
       aceptadas: 2,
       total: 10,
     },
   },
   {
-    nombre: 'Evento 4',
+    nombre: 'Picnic en el parque padre hutardo',
     periodo: '03/01/2023 - 04/01/2023',
-    lugar: 'Lugar 2',
+    lugar: 'Parque La Reina, La Reina asdasds',
     estado: 'Respondido',
     respuestas: {
       aceptadas: 8,
       total: 10,
     },
   },
-  // Agrega más eventos según sea necesario
+  // Agrega más eventos 
 ];
 
 const EventList = () => {
   const renderEventCategory = (category) => (
-    <div key={category} className="mb-5">
+    <div key={category} className="my-5">
       <h2>{category}</h2>
       <div className="">
         <div className="row">
           {eventos
             .filter((event) => event.estado === category)
             .map((event) => (
-              <div key={event.nombre} className="row">
-                <h5 className='col'>{event.nombre}</h5>
-                <p className='col'>{event.periodo.split(' - ')[0]}</p>
-                <p className='col'>{event.lugar}</p>
-                <p className='col'>{`${event.respuestas.aceptadas}/${event.respuestas.total}`}</p>
+              <div key={event.nombre} className="row pb-3">
+                <h5 className='col fw-semibold fs-5 text-truncate'>{event.nombre}</h5>
+                <p className='col fw-semibold fs-5 text-truncate'>{event.periodo.split(' - ')[0]}</p>
+                <p className='col fw-semibold fs-5 text-truncate'>{event.lugar}</p>
+                <p className='col fw-semibold fs-5 text-truncate'>{`${event.respuestas.aceptadas}/${event.respuestas.total}`}</p>
                 <button
-                  className='col m-2 btn btn-outline-primary ' // Modificado aquí
-                  onClick={() => handleEdit(event.nombre)}
+                  className='col m-2 btn btn-outline-primary fs-5  ' 
+                  onClick={() => handleEdit(event.edit)}
                 >
                   Editar
                 </button>
@@ -73,15 +73,15 @@ const EventList = () => {
     </div>
   );
 
-  // Función para manejar la edición del evento (puedes personalizarla según tus necesidades)
+  
   const handleEdit = (eventName) => {
-    // Lógica para la edición del evento
+    
     console.log(`Editar evento: ${eventName}`);
   };
 
-  // Función para manejar la creación de un nuevo evento
+ 
   const handleNewEvent = () => {
-    // Lógica para la creación de un nuevo evento
+  
     console.log('Crear nuevo evento');
   };
 
@@ -91,32 +91,30 @@ const EventList = () => {
         <div className="col-10">
           <h1 className="events-title">EVENTOS</h1>
         </div>
-        <div className="col text-right">
-          <button className="btn btn-success" onClick={() => handleNewEvent()}>Nuevo Evento (0)</button>
+        <div className="mt-2 col text-right ">
+          <button className="btn btn-success fs-5 fw-semibold" onClick={() => handleNewEvent()}>Nuevo Evento (0)</button>
         </div>
       </div>
 
-      {/* Banner o cinta amarilla */}
-      <div className="bg-primary pt-3 mb-3 row container">
+  
+      <div className="bg-primary pt-2 mb-3 row container fw-semibold fs-5">
         <p className='col'>Nombre</p>
-        <p className='col'>Período</p>
-        <p className='col'>Lugar</p>
-        <p className='col'>Respuestas</p>
+        <p className='col px-2'>Período</p>
+        <p className='col ps-5'>Lugar    </p>
+        <p className='col ms-4'>Respuestas</p>
         <p className='col-3'></p>
       </div>
 
-      {/* Contenido principal */}
+    <div className='fw-semibold'>
       {renderEventCategory('Organizado por mí')}
       {renderEventCategory('Respondido')}
-      {renderEventCategory('Pendiente')}
-
-      {/* Línea gris oscuro en la parte inferior */}
-      <hr className="bg-dark" />
-
-      {/* Texto en la parte inferior izquierda */}
+      {renderEventCategory('Pendientes')}
+      </div>
+    
       <div className="row">
         <div className="col">
-          <p className="text-left">Ver eventos pasados y rechazados</p>
+          <div className="divider-line"></div>
+          <p className="text-left text-primary fw-semibold fs-5">Ver eventos pasados y rechazados</p>
         </div>
       </div>
     </div>
